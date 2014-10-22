c39398f4b39923750e2654f2acc6411395cb2397
Dean Gilewicz <gilewiczd@gmail.com>
2014-10-22 14:04:53 -0400

    basic constructor and instances

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,64 @@
-console.log('The Iron Yard Rocks');
+(function () {
+
+
+// CONSTRUCTORS
+
+  // User
+  var Person = function (options) {
+    var options = options || {};
+    this.heatlh = options.health;
+    this.attack = function() {
+      // change health of baddy
+    }
+  };
+
+
+  // Computer
+  var Baddy = function(options) {
+    var options = options || {};
+    this.heatlh = options.health;
+    this.attack = function () {
+      // change health of Player
+    }
+  };
+
+
+  // INSTANCES
+
+  // User
+  var Brutus = new Person ({
+    health: 100
+
+  });
+
+  var Titus = new Person ({
+    health: 100
+
+  });
+
+
+  var Zuchius = new Person ({
+    health: 100
+
+  });
+
+
+  // Computer
+  var Zues = new Baddy ({
+    health: 100
+
+  });
+
+  var Herculus = new Baddy ({
+    health: 500
+
+  });
+
+  var Maximus = new Baddy ({
+    health: 1000
+
+  });
+
+
+
+}());
