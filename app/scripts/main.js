bd4bf72c1a8fef85e6af62c0ede99e302f633843
Dean Gilewicz <gilewiczd@gmail.com>
2014-10-22 16:18:14 -0400

    added attack function

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,4 @@
-(function () {
+// (function () {
 
 
 // CONSTRUCTORS
@@ -7,9 +7,16 @@
   var Goody = function (options) {
     var options = options || {};
     this.name = options.name;
-    this.heatlh = options.health;
-    this.attack = function() {
-      // change health of baddy
+    this.healthMax = options.healthMax || 100;
+    this.currentHealth = this.healthMax;
+    this.attack = function(x) {
+      x.currentHealth = x.currentHealth - (_.random(10,50)); //x is the enemy i.e. hercules
+      if(x.currentHealth <= 0) {
+        console.log("WINNER");
+
+      } else {
+        console.log(x.currentHealth);
+      }
     }
   };
 
@@ -18,9 +25,16 @@
   var Baddy = function(options) {
     var options = options || {};
     this.name = options.name;
-    this.heatlh = options.health;
-    this.attack = function () {
-      // change health of Player
+    this.healthMax = options.healthMax || 100;
+    this.currentHealth = options.healthMax;
+    this.attack = function(x) {
+      x.currentHealth = x.currentHealth - (_.random(10,50)); //x is the enemy i.e. hercules
+      if(x.currentHealth <= 0) {
+        console.log("WINNER");
+
+      } else {
+        console.log(x.currentHealth);
+      }
     }
   };
 
@@ -28,46 +42,39 @@
   // INSTANCES
 
   // User
-  var Brutus = new Goody ({
-    name: 'Brutus',
-    health: 100
+  var brutus = new Goody ({
+    name: 'Brutus'
 
   });
 
-  console.log(Brutus);
-
-  var Titus = new Goody ({
+  var titus = new Goody ({
     name: 'Titus',
-    health: 100
+    healthMax: 200
 
   });
 
 
-  var Zuchius = new Goody ({
+  var zuchius = new Goody ({
     name: 'Zuchius',
-    health: 100
+    healthMax: 300
 
   });
 
 
   // Computer
-  var Zues = new Baddy ({
-    name: 'Zues',
-    health: 100
+  var zeus = new Baddy ({
+    name: 'Zeus'
 
   });
 
-  var Herculus = new Baddy ({
-    name: 'Herculus',
-    health: 500
+  var hercules = new Baddy ({
+    name: 'Hercules',
+    healthMax: 500
 
   });
 
-  var Maximus = new Baddy ({
+  var maximus = new Baddy ({
     name: 'Maximus',
-    health: 1000
+    healthMax: 1000
 
   });
-
-
-}());
