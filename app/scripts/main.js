4112f1e56c32a6862336365100c4ae073fd1753e
Dean Gilewicz <gilewiczd@gmail.com>
2014-10-22 22:40:57 -0400

    added weapons and power up constructors

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,5 @@
 // (function () {
 
-
 // CONSTRUCTORS
 
   // User
@@ -9,18 +8,26 @@
     this.name = options.name;
     this.healthMax = options.healthMax || 100;
     this.currentHealth = this.healthMax;
-    this.attack = function(x) {
-      x.currentHealth = x.currentHealth - (_.random(10,50)); // x is the enemy i.e. hercules
-      if(x.currentHealth <= 0) {
+    this.energyMax = options.eneryMax || 50;
+    this.attack = function(inst) {
+      inst.currentHealth = inst.currentHealth - (_.random(10,50)); // inst - is the enemy i.e. hercules (instance)
+      if(inst.currentHealth <= 0) {
         console.log("WINNER");
 
       } else {
-        console.log(x.currentHealth);
+        console.log(inst.currentHealth);
         this.currentHealth = this.currentHealth - (_.random(10,50));
       }
     }
-  };
+    // this.use = function(inst) {
+    //   inst.currentHealth = inst.currentHealth - (_.random(10,20));
+    // }
 
+    this.healthBoost = Boost;
+    this.take = function(inst) {
+      inst.currentHealth = inst.currentHealth + inst.healthBoost;
+    }
+  };
 
   // Computer
   var Baddy = function(options) {
@@ -28,20 +35,47 @@
     this.name = options.name;
     this.healthMax = options.healthMax || 100;
     this.currentHealth = options.healthMax;
-    this.attack = function(x) { // NO NEED FOR ATTACK FUNCTION IF INCLUDE BADDY ATTACK CODE IN GOODY CONSTRUCTOR
-      x.currentHealth = x.currentHealth - (_.random(10,50)); // x is the goody (whoever you type in that baddy is atttakcing) i.e. brusuts
-      if(x.currentHealth <= 0) {
+    this.energyMax = options.eneryMax || 50;
+    this.attack = function(inst) { // NO NEED FOR ATTACK FUNCTION IF INCLUDE BADDY ATTACK CODE IN GOODY CONSTRUCTOR UNLESS OPTION FOR USER TO PLAY AS GOODY OR BADDY
+      inst.currentHealth = inst.currentHealth - (_.random(10,50)); // x is the goody (whoever you type in that baddy is attakcing) i.e. brutus
+      if(inst.currentHealth <= 0) {
         console.log("WINNER");
 
       } else {
-        console.log(x.currentHealth);
+        console.log(inst.currentHealth);
         this.currentHealth = this.currentHealth - (_.random(10,50));
       }
+    }
+    // this.use = function(inst) {
+    //   inst.currentHealth = inst.currentHealth - (_.random(10,20));
+    // }
+    this.take = function(inst) {
+      inst.currentHealth = inst.currentHealth + juice;
     }
   };
 
 
-  // INSTANCES
+
+
+
+  // Weapons - extra damaage to health or shield
+  var Weapon = function (options) {
+    var options = options || {};
+    this.name = options.name;
+  };
+
+
+
+  // Boost - health or energy
+  var Boost = function (options) {
+    var options = options || {};
+    this.name = options.name;
+    this.healthBoost = options.healthBoost || (_.random(20,30));
+  };
+
+
+
+// INSTANCES
 
   // User
   var brutus = new Goody ({
@@ -78,5 +112,39 @@
   var maximus = new Baddy ({
     name: 'Maximus',
     healthMax: 1000
+
+  });
+
+
+  // Weapons
+  var spear = new Weapon ({
+    name: 'Spear',
+
+  });
+
+  var axe = new Weapon ({
+    name: 'Spear',
+
+  });
+
+  var sword = new Weapon ({
+    name: 'Spear',
+
+  });
+
+
+  // Boost
+  var juice = new Boost ({
+    name: 'Juice',
+
+  });
+
+  var powder = new Boost ({
+    name: 'Powder',
+
+  });
+
+  var herbal = new Boost ({
+    name: 'herbal',
 
   });
