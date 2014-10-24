03284b1380d1c3e5a064f6a83f927b823094512c
Dean Gilewicz <gilewiczd@gmail.com>
2014-10-24 10:45:12 -0400

    updated constructors

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,28 +4,36 @@
 
   // User
   var Goody = function (options) {
+    var  attack_pt, special_pt;
     var options = options || {};
     this.name = options.name;
+    this.type = options.type;
     this.healthMax = options.healthMax || 100;
     this.currentHealth = this.healthMax;
-    this.energyMax = options.eneryMax || 50;
-    this.attack = function(inst) {
-      inst.currentHealth = inst.currentHealth - (_.random(10,50)); // inst - is the enemy i.e. hercules (instance)
-      if(inst.currentHealth <= 0) {
-        console.log("WINNER");
-
-      } else {
-        console.log(inst.currentHealth);
-        this.currentHealth = this.currentHealth - (_.random(10,50));
-      }
-    }
-    // this.use = function(inst) {
-    //   inst.currentHealth = inst.currentHealth - (_.random(10,20));
-    // }
-
-    this.take = function(inst) {
-      inst.currentHealth = inst.currentHealth + inst.healthBoost;
-    }
+    this.healthBoost = options.healthBoost || (_.random(10,30));
+    switch (this.type) {
+      case 1:
+        attack_pt = (_.random(5,20));
+        special_pt = (_.random(20,50));
+      break;
+      case 2:
+        attack_pt = (_.random(10,30));
+        special_pt = (_.random(30,60));
+      break;
+      case 3:
+        attack_pt = (_.random(15,40));
+        special_pt = (_.random(40,70));
+      break;
+    };
+    this.attack = function(attackee) {
+      return attackee.currentHealth = attackee.currentHealth - attack_pt; // attackee - is the enemy - Baddy (instance)
+    };
+    this.special = function (attackee) {
+      return attackee.health = attackee.health - special_pt;
+    };
+    // this.take = function(attackee) {
+    //   attackee.currentHealth = attackee.currentHealth + attackee.healthBoost;
+    // };
 
   };
 
@@ -35,117 +43,135 @@
     this.name = options.name;
     this.healthMax = options.healthMax || 100;
     this.currentHealth = options.healthMax;
-    this.energyMax = options.eneryMax || 50;
-    this.attack = function(inst) { // NO NEED FOR ATTACK FUNCTION IF INCLUDE BADDY ATTACK CODE IN GOODY CONSTRUCTOR UNLESS OPTION FOR USER TO PLAY AS GOODY OR BADDY
-      inst.currentHealth = inst.currentHealth - (_.random(10,50)); // x is the goody (whoever you type in that baddy is attakcing) i.e. brutus
-      if(inst.currentHealth <= 0) {
-        console.log("WINNER");
+    this.attack = function(attackee) {
+      return attackee.currentHealth = attackee.currentHealth - attack_pt; // attackee - is the enemy - Goody (instance)
+    };
+    this.special = function (attackee) {
+      return attackee.health = attackee.health - special_pt;
+    };
 
-      } else {
-        console.log(inst.currentHealth);
-        this.currentHealth = this.currentHealth - (_.random(10,50));
-      }
-    }
-    // this.use = function(inst) {
-    //   inst.currentHealth = inst.currentHealth - (_.random(10,20));
-    // }
-    this.take = function(inst) {
-      this.currentHealth = this.currentHealth + inst.healthBoost; // instance of Goody.currentHealth = instance of Goody.currentHealth + instance of Boost.healthBoost
-    }
-  };
-
-  console.log(inst.healthBoost);
-
-
-
-  // Weapons - extra damaage to health or shield
-  var Weapon = function (options) {
-    var options = options || {};
-    this.name = options.name;
   };
 
 
+  // // Weapons - extra damaage to health or shield
+  // var Weapon = function (options) {
+  //   var options = options || {};
+  //   this.name = options.name;
+  // };
+  //
+  // // Boost - health or energy
+  // var Boost = function (options) {
+  //   var options = options || {};
+  //   this.name = options.name;
+  //   this.healthBoost = options.healthBoost || (_.random(20,30));
+  // };
 
-  // Boost - health or energy
-  var Boost = function (options) {
-    var options = options || {};
-    this.name = options.name;
-    this.healthBoost = options.healthBoost || (_.random(20,30));
+
+
+// STARTING THE GAME
+
+  // // Weapons
+  // var knife = new Weapon ({
+  //   name: 'Knife'
+  //
+  // });
+  //
+  // var nunchucks = new Weapon ({
+  //   name: 'Nunchucks'
+  //
+  // });
+  //
+  // var magic = new Weapon ({
+  //   name: 'Magic'
+  //
+  // });
+  //
+  //
+  // // Boost
+  // var juice = new Boost ({
+  //   name: 'Juice',
+  //   healthBoost: 50
+  //
+  // });
+  //
+  // var powder = new Boost ({
+  //   name: 'Powder'
+  //
+  // });
+  //
+  // var herbal = new Boost ({
+  //   name: 'herbal'
+  //
+  // });
+
+var shinobi,
+    haku,
+    ryu,
+    akemi,
+    yoshiro,
+    takashi;
+
+
+  $('.welcome button').on('click', function (event) {
+    event.preventDefault();
+
+    // User Instances
+    shinobi = new Goody ({ name: 'Shinobi'});
+    // haku = new Goody ({ name: 'Haku', healthMax: 200});
+    // ryu = new Goody ({ name: 'Ryu', healthMax: 300});
+
+    // Computer Instances
+    akemi = new Baddy ({ name: 'Akemi'});
+    // yoshiro = new Baddy ({ name: 'Yoshiro', healthMax: 500});
+    // takashi = new Baddy ({ name: 'Takashi', healthMax: 1000});
+
+
+
+// Get ready to fight
+    $('.welcome').fadeOut(500, function () {
+      $('.ggName').prepend(shinobi.name).find('.ggHealth').text(shinobi.health);
+      $('.bgName').prepend(akemi.name).find('.bgHealth').text(akemi.health);
+      $('.fight').fadeIn();
+    });
+
+  });
+
+
+// FIGHT SEQUENCE
+
+  $('#fight').on('click', function (event) {
+    event.preventDefault();
+
+  var attack_type = _.random(1, 4);
+
+  if (attack_type === 1) {
+    shinobi.attack(akemi);
+  } else {
+    shinobi.special(akemi);
   };
 
+  if (monster.health <= 0) {
+    $('.bgHealth').text(akemi.health);
+  } else {
+    ('.bgHealth').text('0');
+    $('.bgName').css('text-decoration', 'line-through',).css('color', 'red');
+  };
+
+  if (attack_type === 1) {
+    akemi.attack(shinobi);
+  } else {
+    akemi.special(shinobi);
+  };
+
+  if (shinobi.health <= 0) {
+    $('.ggHealth').text(shinobi.health);
+  } else {
+    ('.ggHealth').text('0');
+    $('.ggName').css('text-decoration', 'line-through',).css('color', 'red');
+  };
+
+    // $('#fight').fadeOut();
 
 
-// INSTANCES
 
-  // User
-  var shinobi = new Goody ({
-    name: 'Shinobi'
-
-  });
-
-  var haku = new Goody ({
-    name: 'Haku',
-    healthMax: 200
-
-  });
-
-
-  var ryu = new Goody ({
-    name: 'Ryu',
-    healthMax: 300
-
-  });
-
-
-  // Computer
-  var akemi = new Baddy ({
-    name: 'Akemi'
-
-  });
-
-  var yoshiro = new Baddy ({
-    name: 'Yoshiro',
-    healthMax: 500
-
-  });
-
-  var takashi = new Baddy ({
-    name: 'Takashi',
-    healthMax: 1000
-
-  });
-
-
-  // Weapons
-  var knife = new Weapon ({
-    name: 'Knife'
-
-  });
-
-  var nunchucks = new Weapon ({
-    name: 'Nunchucks'
-
-  });
-
-  var magic = new Weapon ({
-    name: 'Magic'
-
-  });
-
-
-  // Boost
-  var juice = new Boost ({
-    name: 'Juice',
-    healthBoost: 50
-
-  });
-
-  var powder = new Boost ({
-    name: 'Powder'
-
-  });
-
-  var herbal = new Boost ({
-    name: 'herbal'
-
-  });
+});
